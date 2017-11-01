cef12883b7c7c19cf506208ef28834c9a5857b4a
Amin <Amin@amins-MacBook-Pro.local>
2017-11-01 17:08:47 +0330

    json loaded

---
+++ cafe/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Cafe(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    main_image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class CafeImage(models.Model):
+    cafe = models.ForeignKey(to=Cafe, related_name='all_images')
+    image_url = models.URLField()
